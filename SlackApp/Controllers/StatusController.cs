015ecff0422625f5cfdcd64a190459123a20551d
Chris Casson <cassonchris723@gmail.com>
2017-12-11 17:53:37 -0600

    Add project files.

---
+++ SlackApp/Controllers/StatusController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SlackApp.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Status")]
+    public class StatusController : Controller
+    {
+        public IActionResult Get()
+        {
+            return Ok("testing status");
+        }
+    }
+}
