3d4770f76740aa361a6b5e0b1c41a38c7382f8f4
Chris Casson <cassonchris723@gmail.com>
2017-12-12 18:07:39 -0600

    Add config items.

--- SlackApp/Config/TestAppConfig.cs
+++ SlackApp/Config/TestAppConfig.cs
@@ -8,5 +8,8 @@ namespace SlackApp.Config
     public class TestAppConfig
     {
         public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string VerificationToken { get; set; }
+        public string RedirectUri { get; set; }
     }
 }
